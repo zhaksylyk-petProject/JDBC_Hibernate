deab8975b46187af2c02a8bc00f897648fb69c71
Zhaksylyk <nartai53@gmai.com>
2024-06-04 15:23:38 +0600

    added connection manager

--- src/main/java/com/zhaksylyk/Main.java
+++ src/main/java/com/zhaksylyk/Main.java
@@ -1,7 +1,15 @@
 package com.zhaksylyk;
 
+import com.zhaksylyk.util.ConnectionManager;
+import org.postgresql.Driver;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
 }
---
+++ src/main/java/com/zhaksylyk/util/ConnectionManager.java
@@ -0,0 +1,36 @@
+package com.zhaksylyk.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String URL_KEY = "db.url";
+
+    static {
+        LoadDriver();
+    }
+    private ConnectionManager() {
+    }
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void LoadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
